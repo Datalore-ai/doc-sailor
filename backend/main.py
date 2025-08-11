cde40e7b957e7df5f06f640d43df23818f95ed97
SWARAJ-42 <biswalswaraj88@kgpian.iitkgp.ac.in>
2025-08-11 10:10:07 +0530

    streaming response

--- backend/main.py
+++ backend/main.py
@@ -13,15 +13,18 @@ load_dotenv()
 
 app = FastAPI()
 
+
 class SetupRequest(BaseModel):
     link: str
     session_id: Optional[int]
     should_setup_new: bool
 
+
 class QueryRequest(BaseModel):
     query: str
     session_id: int
     max_results: int
+
 
 # Allow CORS for local frontend
 app.add_middleware(
@@ -32,21 +35,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 async def message_stream(msg: str):
     yield f"data: {msg}\n\n"
 
+
 @app.post("/setup_docs")
 async def setup_documentation(request: SetupRequest):
-    print('Session_id: ', request.session_id)
+    print("Session_id: ", request.session_id)
     if request.should_setup_new:
         return StreamingResponse(
             documentation_setup(url=request.link, session_id=request.session_id),
-            media_type="text/event-stream"
+            media_type="text/event-stream",
         )
     elif session_exists(request.session_id):
-        return StreamingResponse(content=message_stream("session exists"), media_type="text/event-stream")
+        return StreamingResponse(
+            content=message_stream("session exists"), media_type="text/event-stream"
+        )
     else:
-        return StreamingResponse(content=message_stream("no session exists"), media_type="text/event-stream")
+        return StreamingResponse(
+            content=message_stream("no session exists"), media_type="text/event-stream"
+        )
+
 
 @app.post("/query_docs")
 async def query_documentation(request: QueryRequest):
@@ -54,6 +64,16 @@ async def query_documentation(request: QueryRequest):
     session_id = request.session_id
     n_points = request.max_results
 
-    results = retrieve_from_store(question=question, session_id=session_id, n_points=n_points)
-    payload = [{"page": {int(r.id)+1}, "content": {r.payload['document']}, "source_link": {r.payload['source_link']}} for r in results]
-    return {"result" : payload}
+    results = retrieve_from_store(
+        question=question, session_id=session_id, n_points=n_points
+    )
+    payload = [
+        {
+            "page": int(r.id) + 1,
+            "content": r.payload["document"],
+            "source_link": r.payload["source_link"],
+        }
+        for r in results
+    ]
+
+    return {"result": payload}
